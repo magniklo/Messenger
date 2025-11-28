ea56da11a947c48e82ecb4eae547092aec7e39a0
Muslimov Rif <progavibe@gmail.com>
2025-11-28 22:22:21 +0300

    Создан класс Message в нем определены поля и конструктор. Есть функции по определнию куда отправляется сообщение и вывод этого

--- src/main/java/ru/itis503/Message.java
+++ src/main/java/ru/itis503/Message.java
@@ -1,5 +1,32 @@
 package ru.itis503;
 
 public class Message {
+    private User user; // от кого
+    private Group group;
+    private Channel channel;
+    private Object object; // кому
+    private String message;
+    private String time;
+
+    public Message(User user, Object object, String message, String time) {
+        this.user = user;
+        this.object = object;
+        this.message = message;
+        this.time = time;
+
+    }
+
+    private  String infoObject() {
+        if (object instanceof User) return ((User) object).getName();
+        else if (object  instanceof Group) return ((Group) object).getName();
+        else return ((Channel) object).getName();
+    }
+
+    public void aboutMessage(){
+        System.out.println("Отправлено сообщение { " +
+                "От - " + user.getName() +
+                ", кому - " + infoObject());
+    }
+
 
 }
